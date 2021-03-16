fd31c55101d0ca08fe11190aa4806ec2315e6e4a
S0n98 <obamababby2015@gmail.com>
2021-03-16 15:08:29 +0700

    edit Ex2

--- algorithm_bai2.py
+++ algorithm_bai2.py
@@ -1,7 +1,7 @@
 if __name__ == '__main__':
     y, x = int(input()), int(input())
     while(y != x):
-        if x * 2 - y <= x - y / 2:
+        if x * 2 - y <= x - y // 2:
             x *= 2
         else:
             x -= 1
