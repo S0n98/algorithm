34dbade7a979733d83df0dd32e934f50ff6b3603
S0n98 <obamababby2015@gmail.com>
2021-03-18 19:07:58 +0700

    add detail step and total step

--- algorithm_bai2.py
+++ algorithm_bai2.py
@@ -1,8 +1,14 @@
 if __name__ == '__main__':
     y, x = int(input()), int(input())
+    count = 0
+    print("All step: ")
     while(y != x):
         if x * 2 - y <= x - y // 2:
             x *= 2
+            print("x * 2")
         else:
             x -= 1
-    print(x, y)
+            print("x - 1")
+        count += 1
+        print(x)
+    print("Total step:", count)
